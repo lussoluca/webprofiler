c733b4157cec6e6e08d734415509ee32b6b85acd
Luca Lusso <llusso@wellnet.it>
2015-08-28 08:26:50 +0200

    Bug fix in request, theme and extension data collectors. Initial implementation of translations data collector

--- src/DataCollector/ExtensionDataCollector.php
+++ src/DataCollector/ExtensionDataCollector.php
@@ -45,19 +45,25 @@ class ExtensionDataCollector extends DataCollector implements DrupalDataCollecto
   public function __construct(ModuleHandlerInterface $module_handler, ThemeHandlerInterface $theme_handler) {
     $this->moduleHandler = $module_handler;
     $this->themeHandler = $theme_handler;
+
+    $this->data['drupal_extension']['modules'] = [];
+    $this->data['drupal_extension']['themes'] = [];
   }
 
   /**
    * {@inheritdoc}
    */
   public function collect(Request $request, Response $response, \Exception $exception = NULL) {
-    $this->data['drupal_extension']['count'] = count($this->moduleHandler->getModuleList()) + count($this->themeHandler->listInfo());
-    $this->data['drupal_extension']['modules'] = $this->moduleHandler->getModuleList();
-    $this->data['drupal_extension']['themes'] = $this->themeHandler->listInfo();
+    $modules = $this->moduleHandler->getModuleList();
+    $themes = $this->themeHandler->listInfo();
+
+    $this->data['drupal_extension']['count'] = count($modules) + count($themes);
+    $this->data['drupal_extension']['modules'] = $modules;
+    $this->data['drupal_extension']['themes'] = $themes;
   }
 
   /**
-   * Returns the total number of drupal extensions.
+   * Returns the total number of active extensions.
    *
    * @return int
    */
@@ -66,63 +72,22 @@ class ExtensionDataCollector extends DataCollector implements DrupalDataCollecto
   }
 
   /**
+   * Returns the total number of active modules.
+   *
    * @return int
    */
   public function countModules() {
-    if (!isset($this->data['drupal_extension']['modules'])) {
-      return 0;
-    }
-
     return count($this->data['drupal_extension']['modules']);
   }
 
   /**
+   * Returns the total number of active themes.
+   *
    * @return int
    */
   public function countThemes() {
-    if (!isset($this->data['drupal_extension']['themes'])) {
-      return 0;
-    }
-
     return count($this->data['drupal_extension']['themes']);
   }
-
-//  /**
-//   * @return array
-//   */
-//  public function moduleInfo() {
-//    if (!isset($this->data['drupal_extension']['modules'])) {
-//      return [];
-//    }
-//
-//    $data = [];
-//    foreach ($this->data['drupal_extension']['modules'] as $module => $info) {
-//      /** @var \Drupal\Core\Extension\Extension $info */
-//      $data[$module] = implode(' | ', [
-//        $this->t('Path: @path', ['@path' => $info->getPathname()]),
-//      ]);
-//    }
-//    return $data;
-//  }
-//
-//  /**
-//   * @return array
-//   */
-//  public function themeInfo() {
-//    if (!isset($this->data['drupal_extension']['themes'])) {
-//      return [];
-//    }
-//    $data = [];
-//    foreach ($this->data['drupal_extension']['themes'] as $name => $info) {
-//      /** @var \Drupal\Core\Extension\Extension $info */
-//      $data[$name] = implode(' | ', [
-//        $this->t('Path: @path', ['@path' => $info->getPathname()]),
-//        $this->t('Status: @status', ['@status' => ($info->status) ? $this->t('enabled') : $this->t('disabled')]),
-//        $this->t('Engine: @engine', ['@engine' => $info->engine]),
-//      ]);
-//    }
-//    return $data;
-//  }
 
   /**
    * {@inheritdoc}
@@ -152,24 +117,34 @@ class ExtensionDataCollector extends DataCollector implements DrupalDataCollecto
     return 'iVBORw0KGgoAAAANSUhEUgAAABQAAAAcCAYAAABh2p9gAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAyRpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADw/eHBhY2tldCBiZWdpbj0i77u/IiBpZD0iVzVNME1wQ2VoaUh6cmVTek5UY3prYzlkIj8+IDx4OnhtcG1ldGEgeG1sbnM6eD0iYWRvYmU6bnM6bWV0YS8iIHg6eG1wdGs9IkFkb2JlIFhNUCBDb3JlIDUuMC1jMDYxIDY0LjE0MDk0OSwgMjAxMC8xMi8wNy0xMDo1NzowMSAgICAgICAgIj4gPHJkZjpSREYgeG1sbnM6cmRmPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1zeW50YXgtbnMjIj4gPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIgeG1sbnM6eG1wPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvIiB4bWxuczp4bXBNTT0iaHR0cDovL25zLmFkb2JlLmNvbS94YXAvMS4wL21tLyIgeG1sbnM6c3RSZWY9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC9zVHlwZS9SZXNvdXJjZVJlZiMiIHhtcDpDcmVhdG9yVG9vbD0iQWRvYmUgUGhvdG9zaG9wIENTNS4xIE1hY2ludG9zaCIgeG1wTU06SW5zdGFuY2VJRD0ieG1wLmlpZDo0RkE1QUM1RjkxNkMxMUUzQjA3OUEzQTNEMUVGMjVDOCIgeG1wTU06RG9jdW1lbnRJRD0ieG1wLmRpZDo0RkE1QUM2MDkxNkMxMUUzQjA3OUEzQTNEMUVGMjVDOCI+IDx4bXBNTTpEZXJpdmVkRnJvbSBzdFJlZjppbnN0YW5jZUlEPSJ4bXAuaWlkOjRGQTVBQzVEOTE2QzExRTNCMDc5QTNBM0QxRUYyNUM4IiBzdFJlZjpkb2N1bWVudElEPSJ4bXAuZGlkOjRGQTVBQzVFOTE2QzExRTNCMDc5QTNBM0QxRUYyNUM4Ii8+IDwvcmRmOkRlc2NyaXB0aW9uPiA8L3JkZjpSREY+IDwveDp4bXBtZXRhPiA8P3hwYWNrZXQgZW5kPSJyIj8+7LqYtwAAAUBJREFUeNpi/P//PwM1ARMDlcGogYPQQBYQ4eDgQIza/SClQNwIxA3okgcOHCDZhRVAfA6I64F4EjW8fBLqwn1AnAvE8aQaqAc1QAZJ7DMQRwPxayDuAGIBYgwUAeL1QHwWGm6XgLgUSf4FEPcAsQQQ+xBjICh8AmARBgSCQNwFxEFIatYCMagQcEHXzGhvb09s6bAdiL2gbHYg/oEm/x8Y00ykRAofEvsvzlgGmsoIpIWBWBaIlYD4Bg61B5DY/ED8HRrjIH2yINchh+E7IH4CxPeBuAAam8jgBBD3IfEtgZgTiPdC9T1BySloYCcQ20DTmTg0tuegWVICjZSNWLMeFgBKKsU45EDi9kA8H4ivEmsgNsAKxEXQBH0TiMtwFg5EgvnQXHIFmibfUGrgeaghTdBIxAoYR2u9EWAgQIABAKKeRzEX0gXIAAAAAElFTkSuQmCC';
   }
 
-//  /**
-//   * {@inheritdoc}
-//   */
-//  public function getPanel() {
-//    $build = array();
-//
-//    // Active modules
-//    $build['modules'] = $this->getTable($this->t('Active modules'), $this->moduleInfo(), array(
-//      $this->t('Key'),
-//      $this->t('Value')
-//    ));
-//
-//    // Active themes
-//    $build['themes'] = $this->getTable($this->t('Active themes'), $this->themeInfo(), array(
-//      $this->t('Key'),
-//      $this->t('Value')
-//    ));
-//
-//    return $build;
-//  }
+  /**
+   * {@inheritdoc}
+   */
+  public function getData() {
+    $data = $this->data;
+
+    // Copy protected properties over public ones to
+    // let json_encode to find them.
+    $this->copyToPublic($data['drupal_extension']['modules']);
+    $this->copyToPublic($data['drupal_extension']['themes']);
+
+    return $data;
+  }
+
+  /**
+   * Copies protected properties to public ones.
+   *
+   * @param \Drupal\Core\Extension\Extension[] $extensions
+   */
+  private function copyToPublic($extensions) {
+    foreach ($extensions as &$extension) {
+      $extension->public_type = $extension->getType();
+      $extension->public_name = $extension->getName();
+      $extension->public_path = $extension->getPath();
+      $extension->public_pathname = $extension->getPathname();
+      $extension->public_filename = $extension->getFilename();
+      $extension->public_extension_pathname = $extension->getExtensionPathname();
+      $extension->public_extension_filename = $extension->getExtensionFilename();
+    }
+  }
 }
---
+++ src/DataCollector/TranslationsDataCollector.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\webprofiler\DataCollector;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Drupal\webprofiler\DrupalDataCollectorInterface;
+use Drupal\webprofiler\StringTranslation\TranslationManagerWrapper;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+
+/**
+ * Class TranslationsDataCollector
+ */
+class TranslationsDataCollector extends DataCollector implements DrupalDataCollectorInterface {
+
+  use StringTranslationTrait, DrupalDataCollectorTrait;
+
+  /**
+   * @var \Drupal\Core\StringTranslation\TranslationInterface
+   */
+  private $translation;
+
+  /**
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $translation
+   */
+  public function __construct(TranslationInterface $translation) {
+    $this->translation = $translation;
+
+    $this->data['translations']['translated'] = [];
+    $this->data['translations']['untranslated'] = [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function collect(Request $request, Response $response, \Exception $exception = NULL) {
+    if($this->translation instanceof TranslationManagerWrapper) {
+      /** \Drupal\webprofiler\StringTranslation\TranslationManagerWrapper $this->translation */
+      $this->data['translations']['translated'] = $this->translation->getTranslated();
+      $this->data['translations']['untranslated'] = $this->translation->getUntranslated();
+    }
+  }
+
+  /**
+   * @return int
+   */
+  public function getTranslatedCount() {
+    return count($this->data['translations']['translated']);
+  }
+
+  /**
+   * @return int
+   */
+  public function getUntranslatedCount() {
+    return count($this->data['translations']['untranslated']);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getName() {
+    return 'translations';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle() {
+    return $this->t('Translations');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIcon() {
+    return 'iVBORw0KGgoAAAANSUhEUgAAABUAAAAcCAYAAACOGPReAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAbElEQVRIx2NgGAXUBowMDAwMaWlp/6ll4KxZsxhZYJy0tDRqGMjAwMDAwEQL77OgCxSXlJBsSG9PDwqfJi6lj/fRvTJ4XYocUTBXE4q8oRtRRBnKwsw8RFw6fA0lKkd1dnYOIpfCCthRMIIAAI0IFu9Hxh7ZAAAAAElFTkSuQmCC';
+  }
+}
